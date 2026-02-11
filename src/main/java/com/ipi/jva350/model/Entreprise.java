2027b31df6533b8af294d7e0e493999892208ad1
kir <maxy69100@gmail.com>
2026-02-11 11:07:38 +0100

    bissextile

--- src/main/java/com/ipi/jva350/model/Entreprise.java
+++ src/main/java/com/ipi/jva350/model/Entreprise.java
@@ -69,18 +69,7 @@ public final class Entreprise {
     }
 
     public static boolean bissextile(int y) {
-        String tmp = String.valueOf(y);
-        if (tmp.charAt(2) == '1' || tmp.charAt(2) == '3' || tmp.charAt(2) == 5 || tmp.charAt(2) == '7' || tmp.charAt(2) == '9') {
-            if (tmp.charAt(3)=='2'||tmp.charAt(3)=='6') return true;
-            else
-                return false;
-        }else{
-            if (tmp.charAt(2) == '0' && tmp.charAt(3) == '0') {
-                return false;
-            }
-            if (tmp.charAt(3)=='0'||tmp.charAt(3)=='4'||tmp.charAt(3)=='8')return true;
-        }
-        return false;
+        return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
     }
 
     public static double proportionPondereeDuMois(LocalDate moisDuConge) {
