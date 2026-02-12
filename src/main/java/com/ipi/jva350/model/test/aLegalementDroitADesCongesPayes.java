5b3361efb01ff578ddbba5cdc5bc336d80951f8f
mki <mohamed.khelifa@edu.igensia.com>
2026-02-12 12:18:55 +0100

    class

--- src/main/java/com/ipi/jva350/model/test/aLegalementDroitADesCongesPayes.java
+++ src/main/java/com/ipi/jva350/model/test/aLegalementDroitADesCongesPayes.java
@@ -1,7 +1,7 @@
 package com.ipi.jva350.model;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;d
 
 public class aLegalementDroitADesCongesPayes {
 
